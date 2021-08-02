87f63db317585b010dd5dfcc06505494333cc3f9
Laura <laura.tracierra@gmail.com>
2021-08-02 15:18:29 -0300

    loading na lista de provas

--- src/components/TestList/TestList.js
+++ src/components/TestList/TestList.js
@@ -5,23 +5,37 @@ import axios from "axios";
 import Professor from "./Professor";
 import Subejct from "./Subject";
 import { Link } from "react-router-dom";
+import Loader from "react-loader-spinner";
 
 export default function TestList(){
     let { type, id, name } = useParams();
     useEffect(() => {
         axios.get(`https://api-respoprovas.herokuapp.com/${type}/${id}/`)
-        .then(success => setTests(success.data))
+        .then(success => {
+            setTests(success.data);
+            setLoading(false);
+        })
         .catch(error => {
             alert("Algo deu errado, tente novamente!");
             console.log(error);
         });
     },[type]);
     const [tests, setTests] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     return(
         <Container>
             <h2>{name[0].toUpperCase() + name.substr(1)}</h2>
+            <Loader 
+                type="TailSpin" 
+                color="#ADBAC7" 
+                height={80} 
+                width={80} 
+                style={{margin: 'auto', display: `${loading? 'block': 'none'}`}}
+            />
             {
+                loading? 
+                null:
                 tests.length === 0?
                 <h6><Link to="/submit">Ainda não temos provas cadastradas, envie uma!</Link></h6>:
                 type === "subject"? 
--- src/components/TestTypes/TestTypes.js
+++ src/components/TestTypes/TestTypes.js
@@ -4,24 +4,36 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Professor from "./Professor";
 import Subject from "./Subject";
+import Loader from "react-loader-spinner";
 
 export default function TestTypes(){
     let { type } = useParams();
 
     useEffect(() => {
         axios.get(`https://api-respoprovas.herokuapp.com/${type}`)
-        .then(success => setList(success.data))
+        .then(success => {
+            setList(success.data);
+            setLoading(false);
+        })
         .catch(error => {
             alert("Algo deu errado, tente novamente!");
             console.log(error);
         });
     },[type]);
     const [list, setList] = useState([]);
+    const [loading, setLoading] = useState(true);
     return(
         <Container>
             <h2>{type === "subject"? "Disciplinas - Ciclo Básico": "Professores"}</h2>
             <List>
                 {type === "professor"? list.map((n,i) => <Professor list={n}/>): <Subject list={list}/>}
+                <Loader 
+                    type="TailSpin" 
+                    color="#ADBAC7" 
+                    height={80} 
+                    width={80} 
+                    style={{margin: 'auto', display: `${loading? 'block': 'none'}`}}
+                />
             </List>
         </Container>
     )
